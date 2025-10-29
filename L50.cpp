2e173e227c2a915d7a0a4cc1d6431482f28eea58
JosueMonterrey <josuemr0206@gmail.com>
2025-10-28 23:18:14 -0600

    Leer entradas en L50.cpp

--- L50.cpp
+++ L50.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main() {
-    
+    int N, M, E;
+    int a, b;
+    cin >> N >> M >> E;
+
+    int personas[N];
+
+    for (int i = 0; i < N; i++) {
+        personas[i] = i;
+    }
+
+    while (M--) {
+        cin >> a >> b;
+        personas[b] = personas[a];
+    }
+
+    while (E--) {
+        cin >> a >> b;
+    }
 }
